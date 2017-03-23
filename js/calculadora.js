c47c0c5930b45b76eebd0675a204d19f154d107d
Edith Mandujano <edithmandujano001@gmail.com>
2017-03-23 16:51:59 -0600

    Calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,15 @@
+
+var entrada = document.getElementById("valor");
+var numero1= entrada;
+console.log(numero1);
+
+function mostrar(valor) {
+	var numero=( entrada.value += valor);
+	return numero;
+	numero1=numero;
+}
+
+function borrar(){
+	var borrar=document.getElementById("valor");
+	borrar.value = "";
+}
